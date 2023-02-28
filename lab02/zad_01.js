a26c6d2cb55a75c9851497363152a0fe2255c2aa
Kacper Hołowaty <kacper.holowaty@gmail.com>
2023-02-28 18:31:25 +0100

    zadania z lab2

--- lab02/zad_01.js
+++ lab02/zad_01.js
@@ -3,12 +3,17 @@
 const numbers = [4, 3, 5, 1, 3, 2, 21, 1, 65, -43, 9];
 
 const helper = [];
+
 for (let i = 0; i < numbers.length; i++) {
-    for (let j = 1; j < numbers.length+1; j++) {
-        if (numbers[i] !== numbers[j]) {
-            helper.push(numbers[i])
+    let czyJest = true
+    for (let j = i+1; j < numbers.length; j++) {
+        if (numbers[i] === numbers[j]) {
+            czyJest = false
         }
     } 
+    if (czyJest === true) {
+        helper.push(numbers[i])
+    }
 }
 console.log(helper);
 
